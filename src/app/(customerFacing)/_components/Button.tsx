d5a6643a84195c3f4eead592b0c393d70abfd912
Artur Chairullin <artur.chairullin@apertia.cz>
2024-10-16 22:20:56 +0200

    Updating Hero Section

---
+++ src/app/(customerFacing)/_components/Button.tsx
@@ -0,0 +1,9 @@
+export default function Button({ text }: { text: string }) {
+  return (
+    <button className="rounded-xl bg-gradient-violet-pink p-[2px]">
+      <div className="flex h-full w-full items-center justify-center bg-gray-700 px-5 py-3 rounded-xl hover:bg-gradient-violet-pink">
+        <p className="text-white">{text}</p>
+      </div>
+    </button>
+  );
+}
--- src/app/(customerFacing)/_components/HeroSection.tsx
+++ src/app/(customerFacing)/_components/HeroSection.tsx
@@ -1,16 +1,44 @@
+import Image from "next/image";
+import Button from "./Button";
+
 export default function HeroSection() {
   return (
-    <section className="w-full bg-gray-900">
-      <div className="container mx-auto flex flex-col gap-4 px-12 pt-36">
-        <h2 className="text-2xl text-white font-light">Plugin for Revit®</h2>
-        <div className="w-16 h-[1px] bg-white"></div>
-        <h1 className="text-4xl font-bold text-transparent bg-gradient-violet-pink bg-clip-text">
-          Elevate Your Designs with Advanced Revit Families
-        </h1>
-        <h2 className="text-2xl text-white font-light">
-          Access a comprehensive library of BIM-integrated families for interior
-          design with the Blocks Plugin
-        </h2>
+    <section>
+      <div className="bg-gray-900 mx-auto w-full">
+        <div className="flex justify-center gap-12 pt-36">
+          <div className="flex flex-col gap-6 w-2/5">
+            <h2 className="text-2xl text-white font-light">
+              Plugin for Revit®
+            </h2>
+            <div className="w-16 h-[1px] bg-white"></div>
+            <h1 className="text-5xl font-bold text-transparent bg-gradient-violet-pink bg-clip-text">
+              Elevate Your Designs with Advanced Revit Families
+            </h1>
+            <h2 className="text-2xl text-white font-light">
+              Access a comprehensive library of BIM-integrated families for
+              interior design with the Blocks Plugin
+            </h2>
+            <div className="flex gap-8 pl-2">
+              <Button text="View Plans" />
+              <Button text="Free Download" />
+            </div>
+          </div>
+          <div className="">
+            <Image
+              src="/hero.png"
+              alt="hero"
+              width={360}
+              height={525}
+              className="pt-26"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center gap-40 py-10 text-lg font-bold text-white bg-black">
+        <h4>Native modeling in Revit</h4>
+        <h4>Parametric families</h4>
+        <h4>Ready-to-render textures</h4>
+        <h4>BIM</h4>
       </div>
     </section>
   );
