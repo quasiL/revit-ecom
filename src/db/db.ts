a4cdd8c22e4edaddee102dec6c729f6e4e51e317
Artur Chairullin <artur.chairullin@apertia.cz>
2024-10-23 22:55:17 +0200

    variable declaration

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,7 +5,7 @@ const prismaClientSingleton = () => {
 };
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+  let prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
 const db = globalThis.prisma ?? prismaClientSingleton();
