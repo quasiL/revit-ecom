7bbc36d838e072d42ce486eebf3241366f7de9f4
Artur Chairullin <artur.chairullin@apertia.cz>
2024-11-17 23:59:53 +0100

    Emails and Admin panel

---
+++ src/email/OrderHistory.tsx
@@ -0,0 +1,79 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+import React from "react";
+
+type OrderHistoryEmailProps = {
+  orders: {
+    id: string;
+    pricePaidInCents: number;
+    createdAt: Date;
+    downloadVerificationId: string;
+    product: {
+      name: string;
+      imagePath: string;
+      description: string;
+    };
+  }[];
+};
+
+OrderHistoryEmail.PreviewProps = {
+  orders: [
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 10000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "Product name",
+        description: "Some description",
+        imagePath: "/products/598b58d0-6c05-4b0b-8b20-25d7f809aa71-111.png",
+      },
+    },
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 2000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "Product name 2",
+        description: "Some other desc",
+        imagePath: "/products/e5745547-5a4a-441b-9cb6-afcf529a4e6b-555.png",
+      },
+    },
+  ],
+} satisfies OrderHistoryEmailProps;
+
+export default function OrderHistoryEmail({ orders }: OrderHistoryEmailProps) {
+  return (
+    <Html>
+      <Preview>Order History & Downloads</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Order History</Heading>
+            {orders.map((order, index) => (
+              <React.Fragment key={order.id}>
+                <OrderInformation
+                  order={order}
+                  product={order.product}
+                  downloadVerificationId={order.downloadVerificationId}
+                />
+                {index < orders.length - 1 && <Hr />}
+              </React.Fragment>
+            ))}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
---
+++ src/email/PurchaseReceipt.tsx
@@ -0,0 +1,59 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+    imagePath: string;
+    description: string;
+  };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: string;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Product name",
+    description: "Some description",
+    imagePath: "/products/0d7a08c5-b35a-4733-aac5-3630fbb52ba0-333.png",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
