d91040533457a60d94af4240fd4bad3fe5b22274
Artur Chairullin <artur.chairullin@apertia.cz>
2024-12-31 13:26:27 +0100

    Rights sign

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,38 +3,47 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-gray-200">
-      <div className="container mx-auto flex items-center justify-between px-6 py-8">
-        <div className="flex gap-8">
-          {[
-            { text: "About", href: "/about" },
-            { text: "FAQ", href: "/faq" },
-            { text: "Terms of Use", href: "/terms" },
-            { text: "Privacy Policy", href: "/privacy" },
-          ].map((item) => (
-            <Link
-              href={item.href}
-              key={item.text}
-              className="hover:text-gray-500"
-            >
-              {item.text}
-            </Link>
-          ))}
+      <div className="container mx-auto px-6 py-8">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-8">
+            {[
+              { text: "About", href: "/about" },
+              { text: "FAQ", href: "/faq" },
+              { text: "Terms of Use", href: "/terms" },
+              { text: "Privacy Policy", href: "/privacy" },
+            ].map((item) => (
+              <Link
+                href={item.href}
+                key={item.text}
+                className="hover:text-gray-500"
+              >
+                {item.text}
+              </Link>
+            ))}
+          </div>
+          <div className="flex gap-4">
+            {[
+              { iconClass: "mgc_linkedin_line", href: "https://linkedin.com" },
+              { iconClass: "mgc_youtube_line", href: "https://youtube.com" },
+              { iconClass: "mgc_social_x_line", href: "https://twitter.com" },
+              { iconClass: "mgc_ins_line", href: "https://instagram.com" },
+              { iconClass: "mgc_facebook_line", href: "https://facebook.com" },
+              { iconClass: "mgc_telegram_line", href: "https://telegram.org" },
+            ].map((item, index) => (
+              <Link
+                href={item.href}
+                key={index}
+                className="hover:text-gray-500"
+              >
+                <span
+                  className={`${item.iconClass} text-3xl text-gray-500`}
+                ></span>
+              </Link>
+            ))}
+          </div>
         </div>
-        <div className="flex gap-4">
-          {[
-            { iconClass: "mgc_linkedin_line", href: "https://linkedin.com" },
-            { iconClass: "mgc_youtube_line", href: "https://youtube.com" },
-            { iconClass: "mgc_social_x_line", href: "https://twitter.com" },
-            { iconClass: "mgc_ins_line", href: "https://instagram.com" },
-            { iconClass: "mgc_facebook_line", href: "https://facebook.com" },
-            { iconClass: "mgc_telegram_line", href: "https://telegram.org" },
-          ].map((item, index) => (
-            <Link href={item.href} key={index} className="hover:text-gray-500">
-              <span
-                className={`${item.iconClass} text-3xl text-gray-500`}
-              ></span>
-            </Link>
-          ))}
+        <div className="text-sm text-gray-600 pt-2">
+          © {new Date().getFullYear()} Simple Revit. All rights reserved.
         </div>
       </div>
     </footer>
