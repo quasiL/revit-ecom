43a55375bdd9973f72e355444877cd540ae97c2d
Artur Chairullin <artur.chairullin@apertia.cz>
2025-03-04 20:02:20 +0100

    Responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-gray-200">
-      <div className="container mx-auto px-6 py-8">
+      <div className="container mx-auto px-4 md:px-6 py-8">
         <div className="flex flex-col md:flex-row items-center md:justify-between gap-4 md:gap-8">
           <div className="flex flex-wrap justify-center md:justify-start gap-4 md:gap-8">
             {[
